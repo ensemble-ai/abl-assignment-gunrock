3b6aeb33733d5582baf6da390a509cfb82e98196
ssdavidson <ssdavidson@ucdavis.edu>
2019-02-02 13:08:49 -0800

    Signed-off-by: ssdavidson <ssdavidson@ucdavis.edu>

---
+++ SimpleABL/src/abl/actions/Move.java
@@ -0,0 +1,25 @@
+package abl.actions;
+
+import game.Bot;
+import game.GameEngine;
+
+import java.awt.Point;
+/**
+ * Sets the trajectory of the chaser. 
+ * 
+ * @author Sam Davidson & Ashwin Bandahar 2-2-19
+ */
+public class Move extends BaseAction {
+
+	/**
+	 * Sets the trajectory of the player to move down.
+	 * args[0] - bot id
+	 */
+	public void execute(Object[] args) {
+		for(Bot b:GameEngine.getInstance().getBots()) {
+			if(b.getId() == (Integer)args[2]) {
+				b.setTrajectory(new Point((Integer)args[0], (Integer)args[1]));
+			}
+		}
+	}
+}
